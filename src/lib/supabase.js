418696695b7640381223a92590eae95ee060b560
nestorw23 <nestor.w23xo@gmail.com>
2025-04-29 19:18:39 -0600

    Reinitialize repository with Edge function and project files

--- src/lib/supabase.js
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://mzuikdkntpvtawwijxpz.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16dWlrZGtudHB2dGF3d2lqeHB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzNTMzMzUsImV4cCI6MjA2MDkyOTMzNX0.ByaRMseDoZiMc0pFbRn-HN03910TRTOeCGnUUHKuBOI'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
